b179e306deecaf904990d8293f3641d99fae008b
Mahmoud-SaadEl-Din <mahmoud.saadeldin101@gmail.com>
2020-04-19 15:13:45 +0200

    Reading input and output with scanf and printf_freopen

---
+++ test.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+freopen("in.txt", "r", stdin);
+freopen("out.txt", "w", stdout);
+
+printf("Hello world 2!\n");
+int a,b;
+int val = scanf("%d %d",&a,&b);
+printf("%d \n", val);
+printf("%d %d\n",a,b);
+
+//string s;
+//getline(cin, s);
+//printf("%s \n", s);
+
+int x;	
+while(cin >> x){
+printf("%d \n", x);	
+		
+}
+
+	
+}
+
