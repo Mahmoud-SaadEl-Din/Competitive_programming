4d7511dc2ff90887a342c9f0ce88cc9fa1079f21
Mahmoud-SaadEl-Din <mahmoud.saadeldin101@gmail.com>
2020-05-29 21:00:27 +0200

    STL

--- test.cpp
+++ test.cpp
@@ -2,10 +2,186 @@
 #include <bits/stdc++.h>
 #include <vector>
 #include<string>
+#include <set>
 
 using namespace std;
 const int MAX = 1000001;
 
+
+///////////////////////////////// STL Part /////////////////////////////////////////
+
+/* Vectors */
+void initialize_vector(int size,int val){
+    // vector of zeros
+    vector<int> v(size);
+    v[0] = 10;
+    v.push_back(50); // create new array
+
+    vector<int> v1 = {1,2,3,4};
+    vector<int> v2(v1); // call the copy constructor
+
+    vector<int> v3(size,val);
+
+    v3.swap(v);
+    
+}
+
+void vec_helping_functions(){
+    vector<int> v;
+    v.push_back(10);
+    v.push_back(20);
+    v.push_back(30);
+    v.pop_back();
+    int ten = v.front();
+    int size = v.size();
+    int capacity = v.capacity();
+    for (int i = 0; i < v.size(); i++)
+    {
+        cout << v[i] << endl;
+    }
+}
+
+void vec_iterator(){
+    vector<int> v = {1,2,3,4};
+    vector<int>::iterator it = v.begin();
+
+    cout << *it;
+    it++;
+    cout << *it;
+
+    vector<int>::iterator it2 = v.end()-1;
+
+    cout << *it;
+    it--;
+    cout << *it;
+
+
+    // vector<int>::iterator it3 = v.begin();
+    vector<int>::reverse_iterator it3 = v.rbegin();
+    for(;it != v.end();it++){
+        cout << *it << endl;
+    }
+
+    auto it4 = v.begin();
+    for (; it4 != v.end(); it4++)
+    {
+        cout << *it << endl;
+    }
+}
+
+void vec_insertion_deletion(int position,int val){
+    vector<int> v = {50, 60, 70};
+    v.insert(v.begin()+position-1,val); // from beginning
+    v.insert(v.end()-position,val); // from last
+    v.emplace(v.begin()+position-1,val); // from beginning
+    v.emplace(v.end()-position,val); // from last
+    v.erase(v.begin()+1); // delete second position
+    v.erase(v.begin()+1, v.end());
+    v.resize(5);
+    v.shrink_to_fit();
+}
+
+bool greater_than_three(int val){
+    return val > 3;
+}
+
+void vec_operation(){
+    vector<int> v = {50, 60, 70};
+    auto pair = minmax(v.begin(),v.end());
+    cout << *pair.first << endl; // min
+    cout << *pair.second << endl; // max
+
+    auto it = min_element(v.begin(), v.end());
+    sort(v.begin(), it); // sort(start_pointer, end_pointer)
+
+    // vector must be sorted
+    sort(v.begin(),v.end());
+    auto it = find_if(v.begin(), v.end(), greater_than_three);
+}
+
+void printandtake_vector(){
+    vector<int> v;
+    copy(istream_iterator<int>(cin), istream_iterator<int>(),back_inserter(v));
+    copy(v.begin(), v.end(), ostream_iterator<int>(cout, " "));
+}
+
+//// Complexity:
+// Add --> Back --> O(1)
+// Delete --> Back --> O(1)
+// Add --> Any Where --> O(N)
+// Delete --> Any Where --> O(N)
+// Access --> [] - at() --> O(1)
+// Search --> find() --> O(log N)
+
+/* End of Vectors */
+
+/* Sets */
+void init_set(){
+    // sorted and unique elements
+    set<int> s {2, 1, 0, 5, 5};
+
+    s.insert(6); // log(N)
+
+    for (auto s:s){
+        cout << s << endl;
+    }
+}
+
+void set_string(){
+    // insert in O(1)
+    set<string> s;
+    auto it = s.begin();
+
+    s.emplace_hint(it, "ahmed");
+    it = s.emplace_hint(it, "belal"); 
+    it = s.emplace_hint(it, "Khaled"); 
+    it = s.emplace_hint(it, "Ali"); 
+    it = s.emplace_hint(it, "yazan");
+
+    for (auto s: s){
+        cout << s << endl;
+    } 
+
+    auto it = s.find("ahmed");
+    s.erase(it); // erase an element
+    s.erase(it, s.end());
+
+    // erase specific range
+    set<int> s1 {10, 20, 30, 40, 50, 60};
+    auto itlow = s1.lower_bound(30);
+    auto itup = s1.upper_bound(60);
+    s1.erase(itlow, itup);
+}
+
+void set_compare(){
+    set<int> s {10, 20, 30, 40, 50, 60};
+    auto com = s.key_comp();
+    auto it = s.begin();
+    int c = 70;
+    for(; com(*it,c); it++){
+        cout << *it << endl;
+    }
+}
+
+void check_existance_set(){
+    set<int> s1 {10, 20, 30, 40, 50, 60};
+    s1.count(20);
+}
+
+void utilies(){
+    set<int> s {10, 20, 30, 40, 50, 60};
+    auto it = s.equal_range(40);
+    cout << *it.first << endl; // print 40
+    cout << *it.second << endl;
+
+}
+
+/* End of Sets */
+
+/* MAP */
+
+/* END OF MAP */
+///////////////////////////////// STL Part /////////////////////////////////////////
 
 bool is_prime(int n){
     if (n < 2) return false;
